619dd6ba8b45b1b1f63bccd56bfdf0856c9f435b
harsh12-art <harshdipb493@gmail.com>
2024-12-04 22:12:22 +0530

    Update dateandtime.js

--- dateandtime.js
+++ dateandtime.js
@@ -1,1 +1,14 @@
-console.log("hellow")
+let N = 3;
+let M = 2;
+let arr = [
+  [1, 2],
+  [3, 4],
+  [5, 6]
+];
+for (let i = 0; i < N; i++) {
+  let rowSum = [];
+  for (let j = 0; j < M; j++) {
+    rowSum.push(i + j);
+  }
+  console.log(rowSum.join(" "));
+}
